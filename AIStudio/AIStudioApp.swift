280ccc56e0ebf14834b574f24c0eda97a759d2ca
Deser7 <deser7@icloud.com>
2026-06-19 18:58:28 +0400

    Создал новый проект

---
+++ AIStudio/AIStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIStudioApp.swift
+//  AIStudio
+//
+//  Created by Наташа Спиридонова on 19.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct AIStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
